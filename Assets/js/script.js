17ceb834b277f2b1550d9653eeae40d734a79fbd
AKJ <kjajila@gmail.com>
2022-10-06 18:12:38 -0400

    add textarea buttons

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,14 @@
 var today=moment().format(("LLLL") );
 $("#currentDay").text(today);
+var currentTime=moment().format("hh");
+//alert(currentTime);
+
+var containerForTime=document.querySelector("container");
+
+for(var i=1;i<25;i++){
+     var textArea = document.createElement("p");
+     console.log(textArea);
+     textArea.textContent="for "+i;
+     //alert(textArea.textContent);
+     body.appendChild(textArea);
+}
