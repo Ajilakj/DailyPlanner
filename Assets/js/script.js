e8076309428b075801b4562c83f5fa07e2aea60a
AKJ <kjajila@gmail.com>
2022-10-06 19:23:35 -0400

    add Am Pm

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,30 +1,32 @@
 var today=moment().format(("LLLL") );
 $("#currentDay").text(today);
 var currentTime=Number(moment().format("hh"));
-alert(currentTime);
+var amPm=moment().format('A');
 
 var containerForTime=document.querySelector("container");
 var divForTime=document.getElementsByClassName("time")
 var divTime=document.getElementsByClassName("time")
 console.dir(divTime);
+
 for(var i=1;i<25;i++){
      // var textArea = document.createElement("p");
-     // console.log(textArea);
      // textArea.textContent="for "+i;
-     // //alert(textArea.textContent);
-     // body.appendChild(textArea);
+     // containerForTime.appendChild(textArea);
+     if(amPm==='PM'){
+          var plus=12;
+     }
+     else{
+          plus=0;
+     }
      var id=Number(divForTime[i-1].id);
-     // console.log(id);
-     if(id<currentTime){
-          
+     var newCurrentTime=currentTime+plus;
+     if(id<newCurrentTime){
           divTime[id-1].classList.add("past");
      }
-     else if(id===currentTime){
-          console.log(id);
+     else if(id===newCurrentTime){
           divTime[id-1].classList.add("present");
      }
      else{
-
           divTime[id-1].classList.add("future");
      }
 }
