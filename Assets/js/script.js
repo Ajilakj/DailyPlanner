eae6a3a161619db733ea22e3c32c5c976db94086
AKJ <kjajila@gmail.com>
2022-10-06 22:00:14 -0400

    store local storage as JSON

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,8 +6,6 @@ var amPm=moment().format('A');
 var containerForTime=document.querySelector("container");
 var divForTime=document.getElementsByClassName("time")
 var divTime=document.getElementsByClassName("time")
-console.dir(divTime);
-
 for(var i=1;i<25;i++){
      // var textArea = document.createElement("p");
      // textArea.textContent="for "+i;
@@ -30,7 +28,30 @@ for(var i=1;i<25;i++){
           divTime[id-1].classList.add("future");
      }
 }
-// var btn=document.querySelector("saveBtn");
-// btn.addEventListener("click",function(){
-//      alert("testing");
-// });
+//btnCollection. length
+var btnCollection = document. getElementsByClassName("saveBtn");
+for (var i = 0; i <btnCollection. length ; i++) {
+     btnCollection[i]. addEventListener("click", function (event) {
+     var parentDiv=$(event.target).parent();
+     //var textContent=parentDiv.children(i).eq(1).text();
+     var textContent=parentDiv.children(i).eq(1).val();
+     console.log(textContent);
+     var schedule={
+          index:i,
+          textContent:textContent
+     };
+     localStorage.setItem("Schedule",JSON.stringify(schedule));
+     });
+}
+function init(){
+     var storedSchedule = JSON.parse(localStorage.getItem("Schedule"));
+     var i=storedSchedule.index;
+     textContent=storedSchedule.textContent;
+     // if (lastGrade !== null) {
+     //      document.getElementById("saved-name").innerHTML = lastGrade.student;
+     //      document.getElementById("saved-grade").innerHTML = lastGrade.grade;
+     //      document.getElementById("saved-comment").innerHTML = lastGrade.comment;
+     //      } else {
+     //        return;
+     //      }
+}
